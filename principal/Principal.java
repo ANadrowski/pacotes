b9c51ea0b950056af5cc03e67c4d35f1510774ce
Aislan Nadrowski <aislan.nadrowski@gmail.com>
2016-06-12 21:40:30 -0300

    Inicio projeto

---
+++ principal/Principal.java
@@ -0,0 +1,14 @@
+package principal;
+
+import uteis.Elemento;
+import java.util.Date;
+
+public class Principal {
+	public static void main(String[] args) {
+	Elemento elemento = new uteis.Elemento();
+
+	System.out.println(elemento);
+
+
+	}
+}
---
+++ uteis/Elemento.java
@@ -0,0 +1,9 @@
+package uteis;
+
+public class Elemento {
+	
+	@Override
+	public String toString() {
+		return "Classe elemento.";
+	}
+}
